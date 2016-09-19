357238852b870ba2be1e1cd7c520f5e2ce60209b
starwarsgood <comp822012016@mail.ru>
2016-09-19 17:23:12 +0300

    19.09.2016

---
+++ index.php
@@ -0,0 +1,19 @@
+<HTML>
+<HEAD>
+</HEAD>
+<BODY>
+<?php
+echo "<B>Ёлементы массива: </B>"."<BR>";
+$lol[10] = "1";
+$lol[20] = "2";
+$lol[30] = "3";
+$lol[40] = "4";
+$lol[50] = "5";
+echo $lol[10]."<br>";
+echo $lol[20]."<br>";
+echo $lol[30]."<br>";
+echo $lol[40]."<br>";
+echo $lol[50]."<br>";
+?>
+</BODY>
+</HTML>
